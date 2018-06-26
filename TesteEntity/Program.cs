41e8365a991bf46fa87405fb8aaf8f3f17ab0385
Vitor Augusto <mendes4212@gmail.com>
2018-06-26 00:20:51 -0300

    Add files via upload

---
+++ TesteEntity/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEntity
+{
+    public class Program
+    {        
+        static void Main(string[] args)
+        {
+            var dbs = DbNames();
+            int soma = 0;
+            string connString = @"Server=DESKTOP-5K9ILHM\SQLEXPRESS;Database={0};User Id=sa; Password=sa@server;";
+            for (int i = 0; i < dbs.Count; i++)
+            { 
+                using(var ctx = new Context(String.Format(connString, dbs[i])))
+                {
+                    soma += ctx.Usuarios.Sum(s => s.Quantidade);
+                }
+
+            }
+            Console.WriteLine("Soma: " + soma);
+            Console.ReadKey();
+        }
+
+        public static List<string> DbNames()
+        {
+            return new List<string>()
+            {
+                "BancoA",
+                "BancoB",
+                "BancoC",
+                "BancoD"
+            };
+        }
+    }
+}
---
+++ TesteEntity/context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEntity
+{
+    public class Context : DbContext
+    {
+        public Context(string conn) : base(conn)
+        {
+
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
